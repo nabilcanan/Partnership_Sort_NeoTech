747558776d0d9b97cf22e0f90cde7f9288156ef8
Nabil Canan <NCanan@sager.com>
2023-09-06 14:29:10 -0400

    Added to main

--- main.py
+++ main.py
@@ -1,152 +1,118 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-import numpy as np
 import pandas as pd
-import sqlite3
-import xlwt
+import webbrowser
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill, Alignment
 
 
 def select_file(title="Select a file"):
     return filedialog.askopenfilename(title=title, filetypes=[("Excel files", "*.xls")])
 
 
-def convert_dtype(value):
-    if isinstance(value, np.generic):
-        return value.item()
-    return value
+def format_headers_in_excel(filename):
+    # Load the workbook
+    workbook = load_workbook(filename)
+
+    headers_to_color = ['PSOFT PART.1', 'PSID CT_y', 'QUOTED MFG_y', 'QUOTED PART_y', 'PART CLASS_y', 'AML CPN_MFGID', 'NAME', 'AML CPN_MFGNUM',
+                        'AML CPN_MFGPARTNUM', 'LIFECYCLE STATUS']
+    color_list = ["fffa9e", "fffa9e", "fffa9e", "fffa9e", "fffa9e", "f3f800", "f3f800", "f3f800", "f3f800", "f3f800"]
+    header_color_mapping = dict(zip(headers_to_color, color_list))
+
+    general_headers_to_color = ['COMPANY', 'PLANT', 'SITE NAME', 'PARTNUM', 'BUYERID', 'IUM', 'PUM', 'NCNR FLAG',
+                                'COMM CODE', 'CUSTOMER PN', 'REFERENCE', 'VENDORNUM', 'VENDORID', 'VENDORNAME',
+                                'CONSOLIDATED NAME', 'CONSOLIDATED NAME 2', 'GROUPDESC', 'MFGNUM', 'MFGID',
+                                'MFGPARTNUM', 'BASEUNITPRICE', 'EFFECTIVEDATE', 'EXPIRATIONDATE', 'CLASSID',
+                                'PURPOINT', 'PARTDESCRIPTION', 'PRODCODE', 'ACTIVE Y OR N', 'CONSOLIDATED CUST NAME',
+                                'MRPSHARE_C', 'ASOF', 'MINORDERQTY', 'MFGLOTMULTIPLE', 'MINMFGLOTSIZE', 'LEADTIME',
+                                'PRICELIST_LT', '90 DAY DEMAND', 'TOTAL DEMAND', 'SUMOFONHANDQTY']
+
+    general_color = "00abee"  # Light blue
+
+    align_wrap = Alignment(wrap_text=True)
+
+    # Loop through all sheets in the workbook
+    for sheet in workbook.worksheets:
+        for cell in sheet[1]:  # Assuming headers are in the first row
+            cell.alignment = align_wrap
+            # Color the specified headers in the "Full File Without Dupes" sheet
+            if sheet.title == 'Full File Without Dupes':
+                if cell.value in header_color_mapping:
+                    cell.fill = PatternFill(start_color=header_color_mapping[cell.value],
+                                            end_color=header_color_mapping[cell.value],
+                                            fill_type="solid")
+                elif cell.value in general_headers_to_color:
+                    cell.fill = PatternFill(start_color=general_color, end_color=general_color, fill_type="solid")
+
+    workbook.save(filename)
 
 
-def combine_workbooks_with_xlwt(target_workbook, new_sheets_data, output_name):
-    with pd.ExcelFile(target_workbook, engine='xlrd') as xls:
-
-        sheet_names = xls.sheet_names
-
-        book = xlwt.Workbook(encoding='utf-8')
-
-        # Write each sheet from the original workbook to the new workbook
-        for sheet in sheet_names:
-            data = pd.read_excel(xls, sheet_name=sheet, engine='xlrd')
-
-            # Add sheet to workbook
-            ws = book.add_sheet(sheet)
-            write_to_excel_sheet(data, ws)
-
-        # Add the new sheets data to the new workbook
-        for sheet_name, sheet_data in new_sheets_data.items():
-            ws = book.add_sheet(sheet_name)
-            write_to_excel_sheet(sheet_data, ws)
-
-        # Save the new workbook
-        book.save(output_name)
-
-
-def write_to_excel_sheet(data, ws):
-    wrapped_style = xlwt.easyxf('align: wrap on')  # Style to wrap text
-
-    # HEADER_COLORS dictionary should be updated to use the correct color codes for `xlwt`.
-    HEADER_COLORS = {
-        'PSOFT PART_y': 0x0D,  # yellow
-        'PSID CT_y': 0x0D,  # yellow
-        'QUOTED MFG_y': 0x0D,  # yellow
-        'QUOTED PART_y': 0x0D,  # yellow
-        'PART CLASS_y': 0x0D  # yellow
-    }
-
-    # Write headers
-    for col_idx, col in enumerate(data.columns):
-        if col in HEADER_COLORS:
-            header_style = xlwt.easyxf('pattern: pattern solid, fore_colour %s; align: wrap on' % HEADER_COLORS[col])
-            ws.write(0, col_idx, col, header_style)
-        else:
-            ws.write(0, col_idx, col, wrapped_style)
-
-    # Write data
-    for row_idx, index in enumerate(data.index):
-        for col_idx, col in enumerate(data.columns):
-            value = data.at[index, col]
-
-            # Convert numpy data types to native Python types
-            value = convert_dtype(value)
-
-            if not pd.isna(value):
-                ws.write(row_idx + 1, col_idx, value)
+def save_to_excel(original_data, unique_data, removed_from_prev_data, output_name):
+    with pd.ExcelWriter(output_name, engine='openpyxl') as writer:
+        original_data.to_excel(writer, sheet_name='Full Original File', index=False)
+        unique_data.to_excel(writer, sheet_name='Full File Without Dupes', index=False)
+        removed_from_prev_data.to_excel(writer, sheet_name='Removed from Prev File', index=False)
 
 
 def compare_neotech():
-    # Prompt user to select last week's file
-    last_week_file = select_file("Select last week's file")
-    if not last_week_file:  # Check if a file was selected
-        return
-
     # Prompt user to select current week's file
     current_week_file = select_file("Select the new file")
-    if not current_week_file:  # Check if a file was selected
+    if not current_week_file:
+        print("Current week's file not selected!")
         return
 
-    # Read the Excel files using xlrd
-    last_week_data = pd.read_excel(last_week_file, sheet_name='Full File', engine='xlrd')
+    # Read the Excel file using pandas with xlrd engine
     current_week_data = pd.read_excel(current_week_file, sheet_name='Sheet1', engine='xlrd')
-
-    # Convert column names to uppercase and strip white spaces
-    last_week_data.columns = last_week_data.columns.str.upper().str.strip()
     current_week_data.columns = current_week_data.columns.str.upper().str.strip()
 
-    # If 'PARTNUM' column not present in either dataframe, raise an error
-    if 'PARTNUM' not in last_week_data.columns or 'PARTNUM' not in current_week_data.columns:
-        raise ValueError("PartNum column not found in one of the files after adjustments.")
-
-    # Process PARTNUM columns
-    last_week_data['PARTNUM'] = last_week_data['PARTNUM'].astype(str).str.strip()
-    current_week_data['PARTNUM'] = current_week_data['PARTNUM'].astype(str).str.strip()
-
-    # Remove duplicates only from last_week_data to ensure we do not add any extra rows to current_week_data
-    # Removing line statement did not help the partnum being brought in as duplicates did not add to the function we
-    # are working with currently
-    last_week_data.drop_duplicates(subset='PARTNUM', inplace=True)
-
-    # Subset the data to merge from last week's data
-    columns_to_merge = ['PARTNUM', 'PSOFT PART', 'PSID CT', 'QUOTED MFG', 'QUOTED PART', 'PART CLASS']
-    data_to_merge = last_week_data[columns_to_merge]
-
-    # Merge the subsetted columns from last week's data into current week's data
-    merged_data = pd.merge(current_week_data, data_to_merge, on='PARTNUM', how='left')
-
-    # Drop the unwanted columns
-    columns_to_drop = ['PSOFT PART_x', 'PSID CT_x', 'QUOTED MFG_x', 'QUOTED PART_x', 'PART CLASS_x']
-    merged_data = merged_data.drop(columns=columns_to_drop)
-
-    # Filter out rows that were removed from the previous week's data
-    removed_from_prev = last_week_data[~last_week_data['PARTNUM'].isin(current_week_data['PARTNUM'])]
-
-    # Create or connect to an SQLite database (this step is kept from your original code, modify if needed)
-    db_conn = sqlite3.connect('neotech_data.db')
-    removed_from_prev.to_sql('removed_from_prev', db_conn, if_exists='replace', index=False)
-
-    # Ask the user to select the workbook into which the new sheets will be added
-    target_workbook = select_file("Choose the workbook where you want to add the new sheets")
-    if not target_workbook:
-        print("No workbook selected to add the sheets.")
+    if 'PARTNUM' not in current_week_data.columns:
+        print("Error: 'PARTNUM' column not found in the current week's file.")
         return
 
-    # Let user specify the output name for the combined workbook
-    output_name = filedialog.asksaveasfilename(title="Save the combined workbook", defaultextension=".xls",
-                                               filetypes=[("Excel files", "*.xls")])
-    if not output_name:
-        print("File save canceled.")
+    # Prompt user to select last week's file
+    last_week_file = select_file("Select last week's file")
+    if not last_week_file:
+        print("Last week's file not selected!")
         return
 
-    # Create a dictionary of sheets to add to the target workbook
-    new_sheets_data = {
-        "Full File": merged_data,
-        "Removed From Prev File": removed_from_prev
-    }
+    last_week_data = pd.read_excel(last_week_file, sheet_name='Full File', engine='xlrd')
+    last_week_data.columns = last_week_data.columns.str.upper().str.strip()  # Convert last_week_data columns too
 
-    combine_workbooks_with_xlwt(target_workbook, new_sheets_data, output_name)
+    # Read the 'Dupes Removed' sheet for merging
+    prev_week_dupes_removed = pd.read_excel(last_week_file, sheet_name='Dupes Removed', engine='xlrd')
+    prev_week_dupes_removed.columns = prev_week_dupes_removed.columns.str.upper().str.strip()
+    print(prev_week_dupes_removed.columns)
 
-    print("Sheets 'Full File' and 'Removed From Prev File' added successfully to", output_name)
-    print("Process complete.")
-    # Show success message
-    messagebox.showinfo("Congrats You're So Smart!", "Success! Final Workbook Saved")
+    # Data from the 2nd file without duplicates
+    unique_data = current_week_data.drop_duplicates(subset='PARTNUM', keep='first')
+
+    # Merge data from prev_week_dupes_removed into unique_data for specified columns
+    columns_to_merge = ['PARTNUM', 'PSOFT PART.1', 'PSID CT', 'QUOTED MFG', 'QUOTED PART', 'PART CLASS']
+    unique_data = unique_data.merge(prev_week_dupes_removed[columns_to_merge], on='PARTNUM', how='left')
+
+    # Columns to drop
+    columns_to_drop_x = ['PSOFT PART', 'PSID CT_x', 'QUOTED MFG_x', 'QUOTED PART_x', 'PART CLASS_x', 'UNNAMED: 40']
+
+    unique_data.drop(columns=columns_to_drop_x, inplace=True)
+
+    # PartNums present in the last week's file but missing in the "Full File Without Dupes"
+    removed_from_prev_data = last_week_data[~last_week_data['PARTNUM'].isin(unique_data['PARTNUM'])]
+
+    # Drop duplicates for the "Removed from Prev File"
+    removed_from_prev_data = removed_from_prev_data.drop_duplicates(subset='PARTNUM', keep='first')
+
+    # Choose the location and name for the output Excel file
+    output_name = filedialog.asksaveasfilename(title="Save the final workbook", defaultextension=".xlsx",
+                                               filetypes=[("Excel files", "*.xlsx")])
+
+    # Save all the dataframes to the Excel file
+    save_to_excel(current_week_data, unique_data, removed_from_prev_data, output_name)
+
+    # Format the headers
+    format_headers_in_excel(output_name)
+
+    # Notify the user of a successful operation using a dialog box
+    messagebox.showinfo("Success", "Operation completed successfully!")
 
 
 # Create the GUI window
@@ -154,6 +120,12 @@ window = tk.Tk()
 
 # Set the window geometry to a larger size
 window.geometry("1200x500")
+
+
+def open_readme_link():
+    webbrowser.open('https://github.com/nabilcanan/Partnership_Sort_NeoTech/blob/main/README.md',
+                    new=2)  # new=2 ensures the link opens in a new window.
+
 
 # Add a title label
 title_label = tk.Label(window, text="Comparing Files For Neotech",
@@ -163,10 +135,9 @@ title_label.grid(row=0, column=0, pady=20)
 # Add instructions label
 instructions_label = tk.Label(window,
                               text="Instructions:\n"
-                                   "1. Select your previous NeoTech Contract File.\n"
-                                   "2. Choose the most recent NeoTech contract File.\n"
-                                   "3. Choose the most recent NeoTech contract File, this is where you're adding the sheet. (Step 2)\n"
-                                   "4. Finally choose where you'd like to save your final workbook\n",
+                                   "1. Select your most recent NeoTech Contract File.\n"
+                                   "2. Select the previous Neotech Contract File.\n"
+                                   "3. Finally choose where you'd like to save your final workbook\n",
                               font=("Microsoft YaHei", 18))
 instructions_label.grid(row=1, column=0, pady=10)
 
@@ -178,6 +149,14 @@ button_frame1.grid(row=2, column=0, pady=10)
 compare_button = tk.Button(button_frame1, text='Compare NeoTech Files', command=compare_neotech,
                            font=("Microsoft YaHei", 20, "bold"), bg="red", fg="white")
 compare_button.pack(fill='both')
+
+# Create a frame for the README link button
+button_frame2 = tk.Frame(window)
+button_frame2.grid(row=3, column=0, pady=10)
+
+readme_button = tk.Button(button_frame2, text='Open README', command=open_readme_link,
+                          font=("Microsoft YaHei", 20), bg="blue", fg="white")
+readme_button.pack(fill='both')
 
 # Configure the grid to expand
 for i in range(6):
